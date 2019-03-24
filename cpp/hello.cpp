ab5602cabbcd1940f4c807fb1c630cab6d5fa4d0
Jedzia <jed69@gmx.de>
2019-03-24 12:03:51 +0100

    added cpp version test build

---
+++ cpp/hello.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    cout << endl;
+    cout << "Hello, Vu+ Duo2!" << endl;
+	
+	float x = 6.0f;
+    cout << "float x = " << x << " divided by 7.0 = " << x / 7.0f << endl;
+
+	double y = 6.0;
+    cout << "double y = " << y << " divided by 7.0 = " << y / 7.0f << endl;
+	
+    return 0;
+}
