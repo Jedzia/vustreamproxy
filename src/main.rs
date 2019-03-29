258510d183c1c6c1e4d6bf7f7dee5229d790463e
Jedzia <jed69@gmx.de>
2019-03-29 08:47:03 +0100

    refactored into functions

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,13 @@
 extern crate futures;
 extern crate hyper;
 extern crate pretty_env_logger;
+#[macro_use]
+extern crate log;
 
 use futures::future;
 use hyper::rt::{Future, Stream};
 use hyper::service::service_fn;
-use hyper::{Body, Method, Request, Response, Server, StatusCode};
+use hyper::{Body, Method, Request, Response, Server, StatusCode, Uri};
 //use hyper::{Client, Server, Method, Body, Response, Request};
 use std::error::Error;
 use std::net::SocketAddr;
@@ -30,16 +32,7 @@ static PANGRAM: &'static str = "the quick brown fox jumped over the lazy dog\n";
 
 //static NTHREADS: i32 = 10;
 
-/// We need to return different futures depending on the route matched,
-/// and we can do that with an enum, such as `futures::Either`, or with
-/// trait objects.
-///
-/// A boxed Future (trait object) is used as it is easier to understand
-/// and extend with more types. Advanced users could switch to `Either`.
-type BoxFut = Box<Future<Item = Response<Body>, Error = hyper::Error> + Send>;
-
-fn get_in_addr() -> SocketAddr
-{
+fn get_in_addr() -> SocketAddr {
     let mut in_addr: SocketAddr = ([192, 168, 3, 43], 3333).into();
 
     if cfg!(target_os = "windows") {
@@ -48,96 +41,52 @@ fn get_in_addr() -> SocketAddr
     return in_addr;
 }
 
+fn reduce_forwarded_uri(uri: &Uri) -> String {
+    //let in_addr: SocketAddr = get_in_addr();
+    let uri_string = uri.path_and_query().map(|x| x.as_str()).unwrap_or("");
+    //let uri: String = uri_string.parse().unwrap();
+
+    //let in_uri_string = format!("http://{}/{}", in_addr, req.uri());
+    let in_remove_string = "/fwd/";
+    debug!("uri_string: {}", uri_string);
+    let result = uri_string.replace(&in_remove_string, "");
+    debug!("result: {}", result);
+
+    //let result = in_uri_string.split(in_remove_string.unwrap_or("")).take(1).next().unwrap_or("");
+    result
+}
+
+/// We need to return different futures depending on the route matched,
+/// and we can do that with an enum, such as `futures::Either`, or with
+/// trait objects.
+///
+/// A boxed Future (trait object) is used as it is easier to understand
+/// and extend with more types. Advanced users could switch to `Either`.
+type BoxFut = Box<Future<Item = Response<Body>, Error = hyper::Error> + Send>;
+
 /// This is our service handler. It receives a Request, routes on its
 /// path, and returns a Future of a Response.
 //fn echo(req: Request<Body>, buf: Vec<u8>) -> BoxFut {
 fn echo(req: Request<Body>) -> BoxFut {
     let mut response = Response::new(Body::empty());
-    println!("method: {}, uri: {}", req.method(), req.uri());
+    debug!("method: {}, uri: {}", req.method(), req.uri());
 
     match req.method() {
         &Method::GET => {
             if req.uri().path().starts_with("/fwd/") {
-                //let in_addr: SocketAddr = get_in_addr();
-                let uri_string = req.uri().path_and_query().map(|x| x.as_str()).unwrap_or("");
-                //let uri: String = uri_string.parse().unwrap();
-
-                //let in_uri_string = format!("http://{}/{}", in_addr, req.uri());
-                let in_remove_string = "/fwd/";
-                println!("uri_string: {}", uri_string);
-                let result = uri_string.replace(&in_remove_string, "");
-                println!("result: {}", result);
-
-                //let result = in_uri_string.split(in_remove_string.unwrap_or("")).take(1).next().unwrap_or("");
-
-                *response.body_mut() = Body::from("Lets forward: ".to_owned() + &result);
-                //*req.uri_mut() = uri;
-                //client.request(req)
+                *response.body_mut() =
+                    Body::from("Lets forward: ".to_owned() + &reduce_forwarded_uri(req.uri()));
             }
-            //*response.body_mut() = Body::from("Jahahahahaha");
         }
-        _ => {
-            //println!("404 not found.");
-            //*response.status_mut() = StatusCode::NOT_FOUND;
-        }
+        _ => {}
     }
 
     match (req.method(), req.uri().path()) {
         // Serve some instructions at /
         (&Method::GET, "/") => {
-            //let mut f = File::open("./p.mp3").expect("failed to open mp3 file!");
-            let mut filepath = "/media/hdd/jedzia/rust/p.mp3";
-            if cfg!(target_os = "windows") {
-               filepath = "p.mp3";
-            }
+            run_pipe_example();
 
-            let mut f = File::open(filepath).expect("failed to open mp3 file!");
-            let mut buffer: Vec<u8> = Vec::new();
-            f.read_to_end(&mut buffer)
-                .expect("failed to read mp3 file.");
-
-
-            let mut wcpath = "wc";
-            if cfg!(target_os = "windows") {
-                wcpath = "C:/msys64/usr/bin/wc.exe";
-            }
-
-            // Spawn the `wc` command
-            let process = match Command::new(wcpath)
-                .stdin(Stdio::piped())
-                .stdout(Stdio::piped())
-                .spawn()
-            {
-                Err(why) => panic!("couldn't spawn wc: {}", why.description()),
-                Ok(process) => process,
-            };
-
-
-            // Write a string to the `stdin` of `wc`.
-            //
-            // `stdin` has type `Option<ChildStdin>`, but since we know this instance
-            // must have one, we can directly `unwrap` it.
-            match process.stdin.unwrap().write_all(PANGRAM.as_bytes()) {
-                Err(why) => panic!("couldn't write to wc stdin: {}",
-                                   why.description()),
-                Ok(_) => println!("sent pangram to wc"),
-            }
-
-            // Because `stdin` does not live after the above calls, it is `drop`ed,
-            // and the pipe is closed.
-            //
-            // This is very important, otherwise `wc` wouldn't start processing the
-            // input we just sent.
-
-            // The `stdout` field also has type `Option<ChildStdout>` so must be unwrapped.
-            let mut s = String::new();
-            match process.stdout.unwrap().read_to_string(&mut s) {
-                Err(why) => panic!("couldn't read wc stdout: {}",
-                                   why.description()),
-                Ok(_) => print!("wc responded with:\n{}", s),
-            }
-
-            *response.body_mut() = Body::from(buffer);
+            *response.body_mut() = Body::from(load_local_mp3_buffer());
 
             //let fbuffer = filebuffer::FileBuffer::open("p.mp3").expect("failed to open file");
             //let rustShitFUCK = fbuffer.leak();
@@ -191,6 +140,62 @@ fn echo(req: Request<Body>) -> BoxFut {
     Box::new(future::ok(response))
 }
 
+fn run_pipe_example() -> () {
+    let mut wcpath = "wc";
+    if cfg!(target_os = "windows") {
+        wcpath = "C:/msys64/usr/bin/wc.exe";
+    }
+
+    // Spawn the `wc` command
+    let process = match Command::new(wcpath)
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()
+    {
+        Err(why) => panic!("couldn't spawn wc: {}", why.description()),
+        Ok(process) => process,
+    };
+
+    // Write a string to the `stdin` of `wc`.
+    //
+    // `stdin` has type `Option<ChildStdin>`, but since we know this instance
+    // must have one, we can directly `unwrap` it.
+    match process.stdin.unwrap().write_all(PANGRAM.as_bytes()) {
+        Err(why) => panic!("couldn't write to wc stdin: {}", why.description()),
+        Ok(_) => println!("sent pangram to wc"),
+    }
+
+    // Because `stdin` does not live after the above calls, it is `drop`ed,
+    // and the pipe is closed.
+    //
+    // This is very important, otherwise `wc` wouldn't start processing the
+    // input we just sent.
+
+    // The `stdout` field also has type `Option<ChildStdout>` so must be unwrapped.
+    let mut s = String::new();
+    match process.stdout.unwrap().read_to_string(&mut s) {
+        Err(why) => panic!("couldn't read wc stdout: {}", why.description()),
+        Ok(_) => print!("wc responded with:\n{}", s),
+    }
+}
+
+fn load_local_mp3_buffer() -> Vec<u8> {
+    //let mut f = File::open("./p.mp3").expect("failed to open mp3 file!");
+    let mut filepath = "/media/hdd/jedzia/rust/p.mp3";
+    if cfg!(target_os = "windows") {
+        filepath = "p.mp3";
+    }
+
+    let mut f = File::open(filepath).expect("failed to open mp3 file!");
+    //let mut f = File::open(filepath);
+    //if let Err(err) = f{
+    //    error!("failed to open mp3 file! {}", err);
+    //}
+    let mut buffer: Vec<u8> = Vec::new();
+    f.read_to_end(&mut buffer)
+        .expect("failed to read mp3 file.");
+    buffer
+}
 //static mut buffer: Vec<u8> = Vec<u8>;
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -269,7 +274,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let server = Server::bind(&in_addr)
         //.serve(|| service_fn(|req| echo(req, Vec::new())))
-        .serve(|| service_fn(echo ))
+        .serve(|| service_fn(echo))
         .map_err(|e| eprintln!("server error: {}", e));
 
     println!("Listening on http://{}", in_addr);
