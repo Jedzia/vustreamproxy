656528368b4ab5c61752d897cfce43efe04dbeba
Jedzia <jed69@gmx.de>
2019-03-30 13:38:09 +0100

    a fucking codec

--- src/chunk.rs
+++ src/chunk.rs
@@ -9,24 +9,30 @@ extern crate tokio_process;
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 use std::path::PathBuf;
 
+use crate::fileio::load_local_mp3_buffer;
+use futures::future::ok;
 use futures::Future;
 use hyper::rt;
 use hyper::service::service_fn;
 use hyper::StatusCode;
 use hyper::{Body, Chunk, Request, Response, Server};
+use std::process::{Command, Stdio};
 use tokio::codec::{Decoder, FramedRead};
 use tokio::fs::File;
 use tokio::io;
-use futures::future::ok;
-use crate::fileio::load_local_mp3_buffer;
-use std::process::{Command, Stdio};
 //use std::error::Error;
-use std::io::{Read, ErrorKind, stdout};
-use core::borrow::BorrowMut;
-use std::error::Error;
-use std::convert::TryInto;
 use self::tokio_process::CommandExt;
+use core::borrow::BorrowMut;
+use std::convert::TryInto;
+use std::error::Error;
+use std::io::{stdout, ErrorKind, Read};
+use self::tokio::prelude::future::IntoFuture;
 
+use tokio::prelude::*;
+use tokio::codec;
+use futures::future;
+use futures::try_ready;
+use bytes::BytesMut;
 
 /*impl From<hyper::Error> for Error {
     fn from(error: hyper::Error) -> Self {
@@ -34,51 +40,127 @@ use self::tokio_process::CommandExt;
     }
 }*/
 
-//pub fn handle_request(req: Request<Body>) -> impl Future<Item = Response<Body>, Error = std::io::Error> {
-pub fn handle_request(req: Request<Body>) -> impl Future<Item = Response<Body>, Error = hyper::Error> {
 
+struct ByteStream<R>(R);
+
+impl <R: AsyncRead> Stream for ByteStream<R> {
+
+    // The same as our future above:
+    type Item = u8;
+    type Error = io::Error;
+
+    // poll is very similar to our Future implementation, except that
+    // it returns an `Option<u8>` instead of a `u8`. This is so that the
+    // Stream can signal that it's finished by returning `None`:
+    fn poll(&mut self) -> Result<Async<Option<u8>>, io::Error> {
+        let mut buf = [0;1];
+        match self.0.poll_read(&mut buf) {
+            Ok(Async::Ready(n)) => {
+                // By convention, if an AsyncRead says that it read 0 bytes,
+                // we should assume that it has got to the end, so we signal that
+                // the Stream is done in this case by returning None:
+                if n == 0 {
+                    Ok(Async::Ready(None))
+                } else {
+                    Ok(Async::Ready(Some(buf[0])))
+                }
+            },
+            Ok(Async::NotReady) => Ok(Async::NotReady),
+            Err(e) => Err(e)
+        }
+    }
+}
+
+
+//pub fn handle_request(req: Request<Body>) -> impl Future<Item = Response<Body>, Error = std::io::Error> {
+pub fn handle_request(
+    req: Request<Body>,
+) -> impl Future<Item = Response<Body>, Error = hyper::Error> {
     //let myfuture = ok::<_, ()>(String::from("hello"));
     //myfuture
 
-    let fuckfuture = ok::<_, hyper::Error>(
-        {
-            //let fff = File::open("p.mp3").map(file_response);
-            /*let fff = File::open("p.mp3").and_then(
+    let fuckfuture = ok::<_, hyper::Error>({
+        //let fff = File::open("p.mp3").map(file_response);
+        /*let fff = File::open("p.mp3").and_then(
 
-                Ok(())
-            );
-            tokio::run(fff);*/
-            //fff.ru;
+            Ok(())
+        );
+        tokio::run(fff);*/
+        //fff.ru;
 
-            let client = File::open("p.mp3")
-                .map(|file| {
-                    let stream = FramedRead::new(file, ChunkDecoder);
+       /* let client = File::open("p.mp3")
+            .map(|file| {
+                let stream = FramedRead::new(file, ChunkDecoder);
+            })
+            .map_err(|err| {
+                // All tasks must have an `Error` type of `()`. This forces error
+                // handling and helps avoid silencing failures.
+                //
+                // In our example, we are only going to log the error to STDOUT.
+                println!("file error = {:?}", err);
+            });*/
 
-                })
-                .map_err(|err| {
-                    // All tasks must have an `Error` type of `()`. This forces error
-                    // handling and helps avoid silencing failures.
-                    //
-                    // In our example, we are only going to log the error to STDOUT.
-                    println!("file error = {:?}", err);
-                });
+        //tokio::spawn(client);
 
-            tokio::spawn(client);
+        //Response::new(Body::from(fff.into()))
+        //Response::new(Body::from("FUCKING HYYYYYYYYYYYYYYPAAAAAAAAAAAAAAAAAAAAAAA"))
 
-            //Response::new(Body::from(fff.into()))
-            Response::new(Body::from("FUCKING HYYYYYYYYYYYYYYPAAAAAAAAAAAAAAAAAAAAAAA"))
-        });
-    //return fuckfuture;
+        //let stream = FramedRead::new(file, ChunkDecoder);
+        //Response::new(Body::wrap_stream(stream))
+  /*      let client2 = File::open("p.mp3")
+            .map(|file| {
+                let stream = FramedRead::new(file, ChunkDecoder);
+            })
+            .map_err(|err| {
+                // All tasks must have an `Error` type of `()`. This forces error
+                // handling and helps avoid silencing failures.
+                //
+                // In our example, we are only going to log the error to STDOUT.
+                println!("file error = {:?}", err);
+            });*/
 
-            //let mut result = Result::Ok(());
+        /*let rotz = || {
+            let file = File::open("p.mp3").map_err(|err| println!("file error = {:?}", err));
+            let stream = FramedRead::new(file.into_future(), ChunkDecoder);
+            stream
+            //Response::new(Body::wrap_stream(stream))
+        };*/
+
+        /*let hurenvotze = || {
+            let data_fuck = vec!["FUCK", " ", "YOU!"];
+            let stream_fuck = futures::stream::iter_ok::<_, ::std::io::Error>(data_fuck);
+            data_fuck
+            //Response::new(Body::wrap_stream(stream))
+        };*/
+
+        let data_fuck = vec!["FUCK", " ", "YOU!"];
+        let stream_fuck = futures::stream::iter_ok::<_, ::std::io::Error>(data_fuck);
+
+        //let byte_stream1 = ByteStream(io::stdin());
+        // via https://jsdw.me/posts/rust-futures-tokio/
+        let byte_stream = codec::FramedRead::new(io::stdin(), FuckingCodec::new())
+        //let byte_stream = codec::FramedRead::new(io::stdin(), ChunkDecoder)
+            // convert our bytes buffer into a stream that emits one byte at a time:
+            .map(|chunk| stream::iter_ok::<_, io::Error>( {
+                //Chunk::from("hahaha")
+                chunk
+            }))
+            // flatten our stream of streams down into one stream:
+            .flatten();
+
+        Response::new(Body::wrap_stream(byte_stream))
+    });
+    return fuckfuture;
+
+    //let mut result = Result::Ok(());
     //let custom_error = Error::new(ErrorKind::Other, "oh no!");
     //let fff: OrElse<Map<OpenFuture<&str>, fn(File) -> Response<Body>>, Result<Response<Body>, Error>, fn(Error) -> Result<Response<Body>, Error>> = File::open("p.mp3").map(file_response)
-//    let fff:Request<Body> = File::open("p.mp3").map(file_response)
-        //.or_else(|_| status_response(hyper::http::StatusCode::NOT_FOUND));
-        //.or_else(|_| ok::<_, hyper::Error>(Response::new(Body::from("ERRrrroooooooooooooooor"))));
-        //.or_else(|e| e.into());
+    //    let fff:Request<Body> = File::open("p.mp3").map(file_response)
+    //.or_else(|_| status_response(hyper::http::StatusCode::NOT_FOUND));
+    //.or_else(|_| ok::<_, hyper::Error>(Response::new(Body::from("ERRrrroooooooooooooooor"))));
+    //.or_else(|e| e.into());
 
-       // .or_else(|e| result.unwrap());
+    // .or_else(|e| result.unwrap());
     ;
 
     //let xxx = fff.into_stream();
@@ -88,101 +170,108 @@ pub fn handle_request(req: Request<Body>) -> impl Future<Item = Response<Body>, 
     //return fff;
 
     /**/
-    let myfuture = ok::<_, hyper::Error>(
-        {
+    let myfuture = ok::<_, hyper::Error>({
+        let command_name = "ffmpeg";
+        //let command_opts = ["-i", "pipe:0", "-f", "mp3", "-acodec", "libvorbis", "-ab", "128k", "-aq", "60", "-f", "ogg", "-"];
+        //"D:\Program Files\ffmpeg\bin\ffmpeg" -re -i "https://cdn.netzpolitik.org/wp-upload/2019/02/NPP169-Worum-geht-es-eigentlich-bei-der-ePrivacy-Reform.ogg"
+        // -acodec libmp3   lame -ab 128k -aq 60 -f mp3 - > bla.mp3
 
+        //let media_addr = "https://cdn.netzpolitik.org/wp-upload/2019/02/NPP169-Worum-geht-es-eigentlich-bei-der-ePrivacy-Reform.ogg";
+        let media_addr = "https://upload.wikimedia.org/wikipedia/commons/f/f2/Median_test.ogg";
+        let command_opts = [
+            "-i",
+            media_addr,
+            "-acodec",
+            "libmp3lame",
+            "-ab",
+            "128k",
+            "-aq",
+            "60",
+            "-f",
+            "mp3",
+            "-",
+        ];
+        let mut ffmpeg_path = command_name;
+        if cfg!(target_os = "windows") {
+            ffmpeg_path = "D:/Program Files/ffmpeg/bin/ffmpeg.exe";
+        }
 
-            let command_name = "ffmpeg";
-            //let command_opts = ["-i", "pipe:0", "-f", "mp3", "-acodec", "libvorbis", "-ab", "128k", "-aq", "60", "-f", "ogg", "-"];
-            //"D:\Program Files\ffmpeg\bin\ffmpeg" -re -i "https://cdn.netzpolitik.org/wp-upload/2019/02/NPP169-Worum-geht-es-eigentlich-bei-der-ePrivacy-Reform.ogg"
-            // -acodec libmp3   lame -ab 128k -aq 60 -f mp3 - > bla.mp3
+        // Use the standard library's `Command` type to build a process and
+        // then execute it via the `CommandExt` trait.
+        let process = Command::new(ffmpeg_path)
+            .args(&command_opts)
+            //.stdin(Stdio::piped())
+            //.stdout(Stdio::piped())
+            .output_async();
+        //{
+        //                    Err(why) => panic!("couldn't spawn {}: {}", command_name, why.description()),
+        //                  Ok(process) => process,
+        //            };
 
-            //let media_addr = "https://cdn.netzpolitik.org/wp-upload/2019/02/NPP169-Worum-geht-es-eigentlich-bei-der-ePrivacy-Reform.ogg";
-            let media_addr = "https://upload.wikimedia.org/wikipedia/commons/f/f2/Median_test.ogg";
-            let command_opts = ["-i", media_addr,
-                "-acodec", "libmp3lame", "-ab", "128k", "-aq", "60", "-f", "mp3", "-"];
-            let mut ffmpeg_path = command_name;
-            if cfg!(target_os = "windows") {
-                ffmpeg_path = "D:/Program Files/ffmpeg/bin/ffmpeg.exe";
-            }
+        use std::io;
+        use std::process::{Command, Output, Stdio};
 
-            // Use the standard library's `Command` type to build a process and
-            // then execute it via the `CommandExt` trait.
-            let process = Command::new(ffmpeg_path)
-                .args(&command_opts)
-                //.stdin(Stdio::piped())
-                //.stdout(Stdio::piped())
-                .output_async();
-                //{
-//                    Err(why) => panic!("couldn't spawn {}: {}", command_name, why.description()),
-  //                  Ok(process) => process,
-    //            };
+        use futures::{Future, Stream};
+        use tokio_process::{Child, CommandExt};
 
-            use std::io;
-            use std::process::{Command, Stdio, Output};
+        fn print_lines(mut cat: Child) -> Box<Future<Item = (), Error = ()> + Send + 'static> {
+            let stdout = cat.stdout().take().unwrap();
+            let reader = io::BufReader::new(stdout);
+            let lines = tokio_io::io::lines(reader);
+            let cycle = lines.for_each(|l| {
+                println!("Line: {}", l);
+                Ok(())
+            });
 
-            use futures::{Future, Stream};
-            use tokio_process::{CommandExt, Child};
+            let future = cycle.join(cat).map(|_| ()).map_err(|e| panic!("{}", e));
 
+            Box::new(future)
+        }
 
-            fn print_lines(mut cat: Child) -> Box<Future<Item = (), Error = ()> + Send + 'static> {
-                let stdout = cat.stdout().take().unwrap();
-                let reader = io::BufReader::new(stdout);
-                let lines = tokio_io::io::lines(reader);
-                let cycle = lines.for_each(|l| {
-                    println!("Line: {}", l);
-                    Ok(())
-                });
+        /*let future = process.map_err(|e| panic!("failed to collect output: {}", e))
+        .map(|output: Output| {
+            //assert!(output.status.success());
+            //assert_eq!(output.stdout, b"hello world\n");
 
-                let future = cycle.join(cat)
-                    .map(|_| ())
-                    .map_err(|e| panic!("{}", e));
+            let stdout = output.stdout;
+            let reader = io::BufReader::new(stdout);
+           // let lines = tokio_io::io::lines(reader);
+           // let cycle = lines.for_each(|l| {
+           //     println!("Line: {}", l);
+          //      Ok(())
+         //   });
 
-                Box::new(future)
-            }
+        });*/
 
+        let mut cmd = Command::new(ffmpeg_path);
+        cmd.args(&command_opts);
+        cmd.stdout(Stdio::piped());
+        let future = print_lines(cmd.spawn_async().expect("failed to spawn command"));
+        tokio::spawn(future);
+        /*// The `stdout` field also has type `Option<ChildStdout>` so must be unwrapped.
+        let mut buffer: Vec<u8> = Vec::new();
+        match process.stdout.unwrap().read_to_end(&mut buffer) {
+            Err(why) => panic!("couldn't read {} stdout: {}", command_name, why.description()),
+            Ok(_) => println!("buffer size:[{}]", buffer.len()),
+        }*/
 
-            /*let future = process.map_err(|e| panic!("failed to collect output: {}", e))
-                .map(|output: Output| {
-                    //assert!(output.status.success());
-                    //assert_eq!(output.stdout, b"hello world\n");
+        //**/response.body_mut() = Body::from(buffer);
+        //return Box::new( future::ok(response));
 
-                    let stdout = output.stdout;
-                    let reader = io::BufReader::new(stdout);
-                   // let lines = tokio_io::io::lines(reader);
-                   // let cycle = lines.for_each(|l| {
-                   //     println!("Line: {}", l);
-                  //      Ok(())
-                 //   });
+        //let stre = process.stdout.unwrap().read_vectored();
 
-                });*/
+        //let stream = FramedRead::new(file, ChunkDecoder);
+        //Response::new(Body::wrap_stream(stream))
 
-            let mut cmd = Command::new(ffmpeg_path);
-            cmd.args(&command_opts);
-            cmd.stdout(Stdio::piped());
-            let future = print_lines(cmd.spawn_async().expect("failed to spawn command"));
-            tokio::spawn(future);
-            /*// The `stdout` field also has type `Option<ChildStdout>` so must be unwrapped.
-            let mut buffer: Vec<u8> = Vec::new();
-            match process.stdout.unwrap().read_to_end(&mut buffer) {
-                Err(why) => panic!("couldn't read {} stdout: {}", command_name, why.description()),
-                Ok(_) => println!("buffer size:[{}]", buffer.len()),
-            }*/
+        //let buffer = load_local_mp3_buffer();
+        //Response::new(Body::from(buffer))
 
-            //**/response.body_mut() = Body::from(buffer);
-            //return Box::new( future::ok(response));
-
-            //let stre = process.stdout.unwrap().read_vectored();
-
-
-
-
-            //let buffer = load_local_mp3_buffer();
-            //Response::new(Body::from(buffer))
-            Response::new(Body::from("tokioooooooo"))
-        });
+        //for i in 0..10 {
+        //}
+        Response::new(Body::from("tokioooooooo"))
+    });
     return myfuture;
-/**/
+    /**/
 
     /*|| {
         Response::new(Body::from("Fuck yaaaaaaaaaaaa"))
@@ -216,7 +305,6 @@ fn extract_path(req: &Request<Body>) -> PathBuf {
     PathBuf::from(req.uri().path().trim_left_matches('/'))
 }
 
-
 struct ChunkDecoder;
 
 impl Decoder for ChunkDecoder {
@@ -227,6 +315,27 @@ impl Decoder for ChunkDecoder {
         let len = buf.len();
         if len > 0 {
             Ok(Some(buf.take().freeze().into()))
+        } else {
+            Ok(None)
+        }
+    }
+}
+
+pub struct FuckingCodec(());
+
+impl FuckingCodec {
+    /// Creates a new `BytesCodec` for shipping around raw bytes.
+    pub fn new() -> FuckingCodec { FuckingCodec(())  }
+}
+
+impl Decoder for FuckingCodec {
+    type Item = BytesMut;
+    type Error = io::Error;
+
+    fn decode(&mut self, buf: &mut BytesMut) -> Result<Option<BytesMut>, io::Error> {
+        if buf.len() > 0 {
+            let len = buf.len();
+            Ok(Some(buf.split_to(len)))
         } else {
             Ok(None)
         }
