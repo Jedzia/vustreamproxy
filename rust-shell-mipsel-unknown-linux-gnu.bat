f4b752c8569db296170562ac24137274142df8d8
Jedzia <jed69@gmx.de>
2019-03-31 16:16:21 +0200

    use windows tool-chain in PATH for mingw-w64 builds

--- rust-shell-mipsel-unknown-linux-gnu.bat
+++ rust-shell-mipsel-unknown-linux-gnu.bat
@@ -1,6 +1,6 @@
 @echo off
 REM set PATH=C:\Toolchain\MIPS\gcc-4.9.2-mips-unknown-linux-gnu\bin;C:\Toolchain\MIPS\gcc-4.9.2-mips-unknown-linux-gnu\libexec\gcc\mips-unknown-linux-gnu\4.9.2;%PATH%
-set PATH=C:\Toolchain\MIPS\mips-2016.05\bin;%PATH%
+set PATH=C:\Toolchain\MIPS\mips-2016.05\bin;C:\Toolchain\mingw-w64\x86_64-6.2.0-posix-seh-rt_v5-rev1\mingw64\bin;%PATH%
 
 set CC=mips-linux-gnu-gcc
 set CFLAGS_mipsel-unknown-linux-gnu=-EL
@@ -14,5 +14,6 @@ echo .
 echo .
 echo cargo build --target=mipsel-unknown-linux-gnu
 echo xargo build --target=mipsel-unknown-linux-gnu
+echo cargo build --target=x86_64-pc-windows-gnu
 echo .
 cmd
