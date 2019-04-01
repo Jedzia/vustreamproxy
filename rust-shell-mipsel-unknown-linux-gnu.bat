239b20a7dc165485527388418234c1b8ad21c1a8
Jedzia <jed69@gmx.de>
2019-04-01 15:58:23 +0200

    local patched openssl-src does the trick of compiling with mipsel

--- rust-shell-mipsel-unknown-linux-gnu.bat
+++ rust-shell-mipsel-unknown-linux-gnu.bat
@@ -1,10 +1,12 @@
 @echo off
 REM set PATH=C:\Toolchain\MIPS\gcc-4.9.2-mips-unknown-linux-gnu\bin;C:\Toolchain\MIPS\gcc-4.9.2-mips-unknown-linux-gnu\libexec\gcc\mips-unknown-linux-gnu\4.9.2;%PATH%
-set PATH=C:\Toolchain\MIPS\mips-2016.05\bin;C:\Toolchain\mingw-w64\x86_64-6.2.0-posix-seh-rt_v5-rev1\mingw64\bin;%PATH%
+set PATH=C:\Toolchain\MIPS\mips-2016.05\bin;C:\Toolchain\mingw-w64\x86_64-6.2.0-posix-seh-rt_v5-rev1\mingw64\bin;C:\msys64\usr\bin;%PATH%
 
 set CC=mips-linux-gnu-gcc
 set CFLAGS_mipsel-unknown-linux-gnu=-EL
 set AR_mipsel-unknown-linux-gnu=mips-linux-gnu-ar
+
+set MAKEFLAGS=-j8
 
 REM call C:\D\ldc2vars64.bat
 rem e:
