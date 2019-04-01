c7c3efa930b6f26ccf0a5bc5b9c13ea943ef672e
Jedzia <jed69@gmx.de>
2019-04-01 16:58:07 +0200

    xargo for mips, cargo for win ... better is to get the openssl-src builder patched

--- CLion 2018.3.2 D Language.bat
+++ CLion 2018.3.2 D Language.bat
@@ -6,4 +6,6 @@ set CC=mips-linux-gnu-gcc
 set CFLAGS_mipsel-unknown-linux-gnu=-EL
 set AR_mipsel-unknown-linux-gnu=mips-linux-gnu-ar
 
+set MAKEFLAGS=-j8
+
 start "" "C:\Program Files\JetBrains\CLion 2018.3.2\bin\clion64.exe"
