70cc63fd26f1f91a457803b784f87a4cb476679c
Jedzia <jed69@gmx.de>
2020-08-23 15:00:22 +0200

    changed target run location

--- run.bat
+++ run.bat
@@ -2,5 +2,5 @@
 @REM SSH="C:/Program Files/Git/usr/bin/ssh.exe" 
 
 
-cp %1 \\VUDUO2X/Harddisk/jedzia/rust
-"C:/Program Files/Git/usr/bin/ssh.exe"  root@vuduo2x /media/hdd/jedzia/rust/vustreamproxy
+cp %1 \\VUDUO2X/Stick01/jedzia/rust
+"C:/Program Files/Git/usr/bin/ssh.exe"  root@vuduo2x /media/hdd/stick01/jedzia/rust/vustreamproxy
